a77f0c1f5acf4eb946ae59d43ca3c793d40d1f73
Joe LeBlanc <contact@jlleblanc.com>
2018-03-02 21:49:31 -0600

    Added support for links

---
+++ src/components/TruffleField/Types/Link.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Link = styled.a`
+  display: block;
+  padding: .5em 0em .5em 0em;
+`
+
+export default ({ value }) => (
+  <Link href={value}>{value}</Link>
+)
--- src/components/TruffleField/Types/index.js
+++ src/components/TruffleField/Types/index.js
@@ -2,10 +2,12 @@ import Comment from './Comment'
 import Tag from './Tag'
 import Plain from './Plain'
 import List from './List'
+import Link from './Link'
 
 let typeMap = {
   'Tag': Tag,
-  'Comment': Comment
+  'Comment': Comment,
+  'Link': Link
 }
 
 class Types {
@@ -20,6 +22,10 @@ class Types {
 
     if (Array.isArray(value)) {
       return List
+    }
+
+    if (value.indexOf('http') === 0) {
+      return Link
     }
 
     return Plain
