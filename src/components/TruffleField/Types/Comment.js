1429e2296b132b485cdc16e4303d7ff28e88b771
Joe LeBlanc <contact@jlleblanc.com>
2018-03-02 19:43:37 -0600

    Comments don't need headers or divs

--- src/components/TruffleField/Types/Comment.js
+++ src/components/TruffleField/Types/Comment.js
@@ -1,10 +1,7 @@
 import React from 'react'
 
 const Comment = ({ value }) => (
-  <div>
-    <h4>Comment</h4>
-    <blockquote>{value}</blockquote>
-  </div>
+  <blockquote>{value}</blockquote>
 )
 
 export default Comment
