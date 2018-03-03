160f365b7559b5e1feebba1e4aa203736dfeba1d
Joe LeBlanc <contact@jlleblanc.com>
2018-03-02 21:52:22 -0600

    Ensure tags are always at the end

--- src/lib/extract-truffle-fields.js
+++ src/lib/extract-truffle-fields.js
@@ -1,11 +1,16 @@
 export default function (truffle) {
   var fields = []
+  var tags = []
 
-  for(var key in truffle) {
-    if(key != 'id' && key != 'hostId' && key != 'timestamp') {
-      fields.push( { type: key, value: truffle[key] } )
+  for (var key in truffle) {
+    if (key != 'id' && key != 'hostId' && key != 'timestamp') {
+      if (typeof(truffle[key]) === 'boolean') {
+        tags.push( { type: key, value: truffle[key] } )
+      } else {
+        fields.push( { type: key, value: truffle[key] } )
+      }
     }
   }
 
-  return fields
+  return fields.concat(tags)
 }
